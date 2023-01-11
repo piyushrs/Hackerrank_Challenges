13efe8f71acf45bd09da6790929c996143c08201
Piyush <piyush2521@gmail.com>
2023-01-11 02:44:24 -0600

    new files in sorting folder

---
+++ Strings/Repeated_strings.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    if s == "a":
+        return n
+    else:
+        while len(s) <= n:
+            s += s
+        if len(s) > n:
+            s = s[:n]
+        
+        return Counter(s)['a']
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
